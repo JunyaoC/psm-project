f617217d2441a4e506708b76b2758ddc77680409
Junyao Chan <junyaoxiandingchan@gmail.com>
2021-05-17 14:39:47 +0800

    Update neo4j.js

--- psm-app/src/neo4j.js
+++ psm-app/src/neo4j.js
@@ -1,8 +1,8 @@
 import neo4j from 'neo4j-driver'
 
 var driver = neo4j.driver(
-  'neo4j://psm.junyaoc.tech:7687',
-  neo4j.auth.basic('neo4j', '123456'),
+  'bolt://psm.junyaoc.tech:7687',
+  neo4j.auth.basic('neo4j', '123456', { encrypted: 'ENCRYPTION_ON' }),
   { disableLosslessIntegers: true }
 )
 
